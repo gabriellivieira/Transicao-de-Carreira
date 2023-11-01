06a8f573a8e15e0bf49b0a6fc9866c11a0e75668
GabiVieira <gabrielli-98@hotmail.com>
2023-10-31 21:52:17 -0300

    Organização SQL e Dashboard Comercial 01

---
+++ SQL/SQL_Server/Secao24_Delimitador_GO.sql
@@ -0,0 +1,20 @@
+/* DELIMITADOR 
+	
+	No MySQL o delimitador padrão era ;
+	Em SQL Server os comandos não precisam de um delimitador
+	porem é recomendado utilizar o 'Go'.
+*/
+
+create database aula_sql
+go
+
+use aula_sql
+go 
+
+create table teste(
+	nome varchar(30)
+)
+go
+
+
+
